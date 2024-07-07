18763b688c463522ff4da07b0507dc003ec2ddce
Ghazal <jamalzadeh@entekhabelectronic.com>
2024-07-07 10:15:55 +0330

    added cover image

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,33 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Container(),
+       body: Stack(
+    children: [
+    Container( // image below the top bar
+    child: SizedBox(
+      height: screenSize.height * 0.45,
+      width: screenSize.width,
+      child: Image.asset(
+        'assets/images/cover.jpeg',
+        fit: BoxFit.cover,
+      ),
+    ),
+    ),
+    Center(
+    heightFactor: 1,
+    child: Padding(
+    padding: EdgeInsets.only(
+    top: screenSize.height * 0.40,
+    left: screenSize.width / 5,
+    right: screenSize.width / 5,
+    ),
+    child: Card( // floating quick access bar
+    // ...
+    ),
+    ),
+    )
+    ],
+    ),
     );
   }
 }
